44f8ef2238da476581897ebb6ec00cd659380e35
TheAkila <nishanakila11@gmail.com>
2026-02-15 23:13:23 +0530

    update

--- next.config.js
+++ next.config.js
@@ -18,6 +18,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'axhbgtkdvghjxtrcvbkc.supabase.co',
       },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
     ],
     formats: ['image/avif', 'image/webp'],
   },
